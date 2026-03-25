e1d5ed919e72ef9f1569c9fa9d611f565aa9bf4f
Reda Lemeden <git@redalemeden.com>
2026-03-25 21:48:12 +0100

    Add hex string initializer to SwiftUI Color (#2)
    * Update to Swift 6 and fix tvOS platform deprecation
    * Add hex string initializer to SwiftUI Color
    * Update CI to use macOS 15 runner and checkout v4

--- Sources/Kroma/SwiftUI.Color.swift
+++ Sources/Kroma/SwiftUI.Color.swift
@@ -12,6 +12,44 @@ extension Color {
   public var raw: CGColor {
     cgColor ?? PlatformColor(self).cgColor
   }
+
+  public init(hex: String) {
+    var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+    if hexSanitized.hasPrefix("#") {
+      hexSanitized.removeFirst()
+    }
+
+    var rgb: UInt64 = 0
+    Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+    let length = hexSanitized.count
+    let r, g, b, a: Double
+
+    switch length {
+    case 3:
+      r = Double((rgb >> 8) & 0xF) / 15.0
+      g = Double((rgb >> 4) & 0xF) / 15.0
+      b = Double(rgb & 0xF) / 15.0
+      a = 1.0
+    case 6:
+      r = Double((rgb >> 16) & 0xFF) / 255.0
+      g = Double((rgb >> 8) & 0xFF) / 255.0
+      b = Double(rgb & 0xFF) / 255.0
+      a = 1.0
+    case 8:
+      r = Double((rgb >> 24) & 0xFF) / 255.0
+      g = Double((rgb >> 16) & 0xFF) / 255.0
+      b = Double((rgb >> 8) & 0xFF) / 255.0
+      a = Double(rgb & 0xFF) / 255.0
+    default:
+      r = 0
+      g = 0
+      b = 0
+      a = 1.0
+    }
+
+    self.init(red: r, green: g, blue: b, opacity: a)
+  }
 }
 
 // MARK: - RGB
